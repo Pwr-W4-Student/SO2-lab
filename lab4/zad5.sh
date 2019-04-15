a484b0e1bf3eee3ccbee4b125cd2b2cb0fcd8823
Pawel_dev <twardawa.pawel@gmail.com>
2019-04-15 11:36:10 +0200

    lab4

--- lab4/zad5.sh
+++ lab4/zad5.sh
@@ -3,7 +3,7 @@
 find ./ \( -name  "*.txt" -o -name "*.dat" \) -type f  -cmin -5 |
 while read file
 do
-    echo "`realpath $file` | `date '+%d-%m-%Y | %H:%M:%S'`"  | tee -a result
+    echo "`realpath $file` | `date '+%d-%m-%Y | %H:%M:%S'`" $file | tee -a result
 	
 done 
 
